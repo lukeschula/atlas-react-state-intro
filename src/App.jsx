8e19324f04257b88eee8be018cc7de6524112ea5
lukeschula <schulaluke@gmail.com>
2025-01-15 22:36:15 -0600

    Adding the rest of refactored code working on adding and dropping

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
+import { useState } from "react";
+import { createContext } from "react";
+
+export const AppContext = createContext();
 
 export default function App() {
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
   return (
-    <div>
-      <Header />
-      <SchoolCatalog />
-      <ClassSchedule />
-    </div>
+    <AppContext.Provider value={{ enrolledCourses, setEnrolledCourses }}>
+      <div>
+        <Header />
+        <SchoolCatalog />
+        <ClassSchedule />
+      </div>
+    </AppContext.Provider>
   );
 }
--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -1,12 +1,13 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "./App";
 
 export default function SchoolCatalog() {
   const [courses, setCourses] = useState([]);
   const [filter, setFilter] = useState("");
   const [sort, setSort] = useState("trimester");
   const [direction, setDirection] = useState("asc");
-  const [page, SetPage] = useState(1);
+  const [page, setPage] = useState(1);
+  const { enrolledCourses, setEnrolledCourses } = useContext(AppContext);
 
   useEffect(() => {
     fetch("api/courses.json")
@@ -20,9 +21,9 @@ export default function SchoolCatalog() {
       course.courseNumber.toLowerCase().startsWith(filter.toLowerCase())
   );
 
-  const handleSort = (field) => {
+  const handleSort = (column) => {
     const newOrder = sort == field && direction === "asc" ? "desc" : "asc";
-    setSort(field);
+    setSort(column);
     setDirection(newOrder);
   };
 
@@ -61,12 +62,15 @@ export default function SchoolCatalog() {
     return sortElement;
   });
 
-  const currentPage = sortedCourses.slice(
-    (page - 1) * PAGE_SIZE,
-    page * PAGE_SIZE
-  );
-  const hasMore = sortedCourses.length > page * PAGE_SIZE;
+  const currentPage = sortedCourses.slice((page - 1) * 5, page * 5);
+  const hasMore = sortedCourses.length > page * 5;
   const hasLess = page > 1;
+
+  function addCourse(course) {
+    const newCourses = [...enrolledCourses];
+    newCourses.push(course);
+    setEnrolledCourses(newCourses);
+  }
 
   return (
     <div className="school-catalog">
@@ -113,41 +117,27 @@ export default function SchoolCatalog() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>PP1000</td>
-            <td>Beginning Procedural Programming</td>
-            <td>2</td>
-            <td>30</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>PP1100</td>
-            <td>Basic Procedural Programming</td>
-            <td>4</td>
-            <td>50</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>OS1000</td>
-            <td>Fundamentals of Open Source Operating Systems</td>
-            <td>2.5</td>
-            <td>37.5</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
+          {currentPage.map((course) => (
+            <tr key={course.courseNumber}>
+              <td>{course.trimester}</td>
+              <td>{course.courseNumber}</td>
+              <td>{course.courseName}</td>
+              <td>{course.semesterCredits}</td>
+              <td>{course.totalClockHours}</td>
+              <td>
+                <button onClick={() => addCourse(course)}>Enroll</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="pagination">
-        <button>Previous</button>
-        <button>Next</button>
+        <button disabled={!hasLess} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <button disabled={!hasMore} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
       </div>
     </div>
   );
